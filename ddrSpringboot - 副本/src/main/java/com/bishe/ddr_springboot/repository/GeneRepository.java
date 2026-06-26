370115c4c9faab42f96f9c6eb5dcf00ab1d34805
isacaiji <115918956+isacaiji@users.noreply.github.com>
2026-06-26 09:23:43 +0800

    Delete ddrSpringboot - 副本 directory

--- ddrSpringboot - 副本/src/main/java/com/bishe/ddr_springboot/repository/GeneRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bishe.ddr_springboot.repository;
-
-import com.bishe.ddr_springboot.entity.Gene;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GeneRepository extends JpaRepository<Gene, Integer> {
-}
--- ddrSpringboot - 副本/src/main/java/com/bishe/ddr_springboot/service/network/NetworkServiceImpl.java
+++
@@ -1,4 +0,0 @@
-package com.bishe.ddr_springboot.service.network;
-
-public class NetworkServiceImpl {
-}
